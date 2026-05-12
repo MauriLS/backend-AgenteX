906513f63a8ae34d301821e1542dc769d076e2e2
MauriLS <mauriciolillo122@gmail.com>
2026-05-12 00:41:14 -0400

    feat(register): crear vista de alta empresarial con selección de agente

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,10 +1,14 @@
-// backend/routes/auth.routes.js
 const express = require('express');
 const router = express.Router();
-const authController = require('../controllers/auth.controller');
 
-// Definimos los endpoints y los conectamos con su función correspondiente
-router.post('/register', authController.register);
-router.post('/login', authController.login);
+// 1. IMPORTACIÓN CORREGIDA (Sin llaves, porque exportas directo)
+const verifyToken = require('../middlewares/auth'); 
+
+const { registerB2B, login } = require('../controllers/auth.controller');
+
+// 3. LAS RUTAS
+router.post('/login', login);
+// OJO: Cambiaste la ruta de /register a /auth. Lo dejaremos así, pero recuérdalo para el Frontend.
+router.post('/register', verifyToken, registerB2B);
 
 module.exports = router;
